d6effec76a56728b8a9160e825b38cc70499150b
Lauren <lauren.finley1@yahoo.com>
2018-04-14 21:54:17 -0500

    Registration Activity Test

---
+++ MyFirebaseApp/app/src/test/java/com/example/dipapc/myfirebaseapp/RegistrationActivityTest.java
@@ -0,0 +1,14 @@
+package com.example.dipapc.myfirebaseapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegistrationActivityTest {
+
+    @Test
+    public void onCreate() {
+
+
+    }
+}
